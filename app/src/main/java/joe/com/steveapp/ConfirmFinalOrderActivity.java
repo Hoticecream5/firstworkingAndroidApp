f1bdca4e026074ada74f76a0991b7a549141d78b
Hoticecream5 <mp18797@gmail.com>
2020-02-10 18:35:29 +0200

    adding customers to database

--- app/src/main/java/joe/com/steveapp/ConfirmFinalOrderActivity.java
+++ app/src/main/java/joe/com/steveapp/ConfirmFinalOrderActivity.java
@@ -25,20 +25,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
+import java.io.Serializable;
 import java.lang.reflect.Member;
+import java.util.HashMap;
 
 import joe.com.steveapp.Prevalent.Prevalent;
 
-public class ConfirmFinalOrderActivity extends AppCompatActivity {
+public class ConfirmFinalOrderActivity extends AppCompatActivity implements Serializable {
+    static final long serialUID = 40L;
     private Button confirmOrderBtn;
-    private EditText userNameTxt,userNumber;
-    private String name,phone_Number,finalMessage, productPrice,
-            orderNumbr,hseNum,addrs,blkNum,resName,roomNum
-            ,rest_Address,user_Address, rest_Name, productName;
+    private EditText userNameTxt, userNumber;
+    private String name, phone_Number, finalMessage, productPrice,
+            orderNumbr, hseNum, addrs, blkNum, resName, roomNum, rest_Address, user_Address, rest_Name, productName;
     private int count;
     DatabaseReference reff;
     Customers customers;
-    long maxId = 0 ;
+    long maxId = 0;
 
 
     @Override
@@ -55,7 +57,7 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
         reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     maxId = (dataSnapshot.getChildrenCount());
                 }
             }
@@ -85,7 +87,7 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
         confirmOrderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                messageToSend();
+        messageToSend();
             }
         });
 
@@ -125,7 +127,8 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
         name = userNameTxt.getText().toString();
         phone_Number = userNumber.getText().toString();
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(phone_Number)) {
-            if (TextUtils.isEmpty(orderNumbr) && TextUtils.isEmpty(resName)) {
+            Toast.makeText(this, "Please enter name and phone number ", Toast.LENGTH_SHORT).show();
+          /*  if (TextUtils.isEmpty(orderNumbr) && TextUtils.isEmpty(resName)) {
                 finalMessage = "Name: " + name + "\n" +
                         "Phone Number: " + phone_Number + "\n" +
                         "Address: " + addrs + "\n" +
@@ -168,38 +171,160 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
                         "Order Number " + orderNumbr;
 
             }
-            customers.setFinalMessage(finalMessage);
-           // reff.child(String.valueOf(maxId + 1)).setValue(customers);
-            reff.push().setValue(customers);
+            //reff.child(String.valueOf(maxId + 1)).setValue(customers);
+            //Intent intent = new Intent(this, AllDoneActivity.class);*/
+
+            if (name == null) {
+                customers.setName(" ");
+            } else {
+                customers.setName(name);
+            }
+            if (phone_Number == null) {
+                customers.setPhone_Number(" ");
+            } else {
+                customers.setPhone_Number(phone_Number);
+            }
+
+            if ((productName == null)) {
+                customers.setProductName(" ");
+            } else {
+                customers.setProductName(productName);
+            }
+            if (resName == null) {
+                customers.setResName(" ");
+            } else {
+                customers.setResName(resName);
+            }
+            if (rest_Address == null) {
+                customers.setRest_Address(" ");
+            } else {
+                customers.setRest_Address(rest_Address);
+            }
+            if (rest_Name == null) {
+                customers.setResName(" ");
+            } else {
+                customers.setRest_Name(rest_Name);
+            }
+
+            if (roomNum == null) {
+                customers.setRoomNum(" ");
+            } else {
+                customers.setRoomNum(roomNum);
+            }
+            if (blkNum == null) {
+                customers.setBlkNum(" ");
+            } else {
+                customers.setBlkNum(blkNum);
+            }
+            if (productPrice == null) {
+                customers.setProductPrice(" ");
+            } else {
+                customers.setProductPrice(productPrice);
+            }
+            if (orderNumbr == null) {
+                customers.setOrderNumbr(" ");
+            } else {
+                customers.setOrderNumbr(orderNumbr);
+            }
+
+            if (addrs == null) {
+                customers.setAddrs(" ");
+            } else {
+                customers.setAddrs(addrs);
+            }
+
+            if (hseNum == null) {
+                customers.setHseNum(" ");
+            } else {
+                customers.setHseNum(hseNum);
+            }
+            if (String.valueOf(count) == null) {
+                customers.setCount(" ");
+            } else {
+                customers.setCount(String.valueOf(count));
+            }
+            if (user_Address == null) {
+                customers.setUser_Address(" ");
+            } else {
+                customers.setUser_Address(user_Address);
+            }
+
+            final DatabaseReference RootRef;
+            RootRef = FirebaseDatabase.getInstance().getReference();
+
+            reff.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        maxId = (dataSnapshot.getChildrenCount());
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (!(dataSnapshot.child("Customers").child(name).exists())) {
+                        HashMap<String, Object> userdataMap = new HashMap<>();
+                        userdataMap.put("Name", name);
+                        userdataMap.put("Phone_Number", phone_Number);
+                        userdataMap.put("Product_Name", productName);
+                        userdataMap.put("Residnce Name", resName);
+                        userdataMap.put("Room Number", roomNum);
+                        userdataMap.put("Block Number", blkNum);
+                        userdataMap.put("Order", orderNumbr);
+                        userdataMap.put("Addrress", addrs);
+                        userdataMap.put("House Number", hseNum);
+                        userdataMap.put("User Address", user_Address);
+                        userdataMap.put("Restaurant Address", rest_Address);
+                        userdataMap.put("Restaurant Name", rest_Name);
+                        userdataMap.put("Number of Rides", count);
+                        userdataMap.put("Product Price", productPrice);
+
+
+                        RootRef.child("Customers").child(name).updateChildren(userdataMap)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(ConfirmFinalOrderActivity.this, "Driver Alerted.", Toast.LENGTH_SHORT).show();
+
+
+                                            Intent intent = new Intent(ConfirmFinalOrderActivity.this, AllDoneActivity.class);
+                                            startActivity(intent);
+
+                                        } else {
+
+                                            Toast.makeText(ConfirmFinalOrderActivity.this, "Network Error: Make sure you are connected to the internet...", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(ConfirmFinalOrderActivity.this, "This " + finalMessage + " already exists.", Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(ConfirmFinalOrderActivity.this, "Please try again using another Email address.", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(ConfirmFinalOrderActivity.this, ConfirmFinalOrderActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
 
         }
     }
-
-            /*    SmsManager smsManager=SmsManager.getDefault();
-                smsManager.sendTextMessage(drvNumber,null,finalMessage,null,null);
-
-            }
-        emptyCart();
-    }*/
-
-    public void emptyCart(){
-
-        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
-
-        cartListRef.child("User View")
-                .child(Prevalent.currentOnlineUser.getPhone())
-                .child("Products")
-                .removeValue()
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task)
-                    {
-                        if (task.isSuccessful())
-                        {
-                        }
-                    }
-                });
-    }
-
 }
+
+
+
 
--- app/src/main/java/joe/com/steveapp/Customers.java
+++ app/src/main/java/joe/com/steveapp/Customers.java
@@ -1,21 +1,124 @@
 package joe.com.steveapp;
 
 public class Customers {
-    public String finalMessage;
+    public String name,phone_Number,resName ,
+            blkNum,productName , productPrice , count, roomNum,
+            user_Address ,rest_Address , rest_Name, orderNumbr
+            ,hseNum,addrs;
 
-    public  Customers(){
-
+    public Customers() {
     }
 
-    public Customers(String finalMessage) {
-        this.finalMessage = finalMessage;
+
+    public String getName() {
+        return name;
     }
 
-    public String getFinalMessage() {
-        return finalMessage;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setFinalMessage(String finalMessage) {
-        this.finalMessage = finalMessage;
+    public String getPhone_Number() {
+        return phone_Number;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        this.phone_Number = phone_Number;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getBlkNum() {
+        return blkNum;
+    }
+
+    public void setBlkNum(String blkNum) {
+        this.blkNum = blkNum;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(String roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public String getUser_Address() {
+        return user_Address;
+    }
+
+    public void setUser_Address(String user_Address) {
+        this.user_Address = user_Address;
+    }
+
+    public String getRest_Address() {
+        return rest_Address;
+    }
+
+    public void setRest_Address(String rest_Address) {
+        this.rest_Address = rest_Address;
+    }
+
+    public String getRest_Name() {
+        return rest_Name;
+    }
+
+    public void setRest_Name(String rest_Name) {
+        this.rest_Name = rest_Name;
+    }
+
+    public String getOrderNumbr() {
+        return orderNumbr;
+    }
+
+    public void setOrderNumbr(String orderNumbr) {
+        this.orderNumbr = orderNumbr;
+    }
+
+    public String getHseNum() {
+        return hseNum;
+    }
+
+    public void setHseNum(String hseNum) {
+        this.hseNum = hseNum;
+    }
+
+    public String getAddrs() {
+        return addrs;
+    }
+
+    public void setAddrs(String addrs) {
+        this.addrs = addrs;
     }
 }
